11afed9db4122145b5f3d84102165a4180d63729
Mehulpatangya <mehuljainind@gmail.com>
2022-06-21 22:07:50 +0530

    care safely added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Assistance from './components/Assistance';
 import Features from './components/Features';
+import Caresafely from './components/Caresafely'
 function App() {
   return (
     <div>
       <h3> Hospital CLone</h3>
       <Features />
       <Assistance />
+      <Caresafely />
     </div>
 
   );
---
+++ src/components/Caresafely.js
@@ -0,0 +1,46 @@
+import { Card, CardContent, CardActionArea, Container, Grid, Typography } from '@mui/material'
+import React from 'react'
+
+function Caresafely() {
+    return (
+        
+        <div style={{backgroundColor: '#f5f5f5'}}>
+            <Container maxWidth="lg" sx={{ p: 6 , backgroundColor: '#f5f5f5'}}>
+            <Typography  component="div" sx={{ pb: 4, textAlign: 'center', fontSize: '2.2rem' }}>Return to Care Safely</Typography>
+            <Grid container spacing={3} sx={{ alignItems: 'center', px: 2 }}>
+                <Grid item xs={4}>
+                    <CardActionArea>
+                        <Card sx={{ height: 200, p: 2 }}>
+
+                            <CardContent sx={{ p: 2, textAlign: "center" }}>
+                                <img src="https://www.hopkinsmedicine.org/_images/_svg/info-circle-blue.svg" alt="information" height={125} width={125} />
+                                <Typography variant="h5" component="div" sx={{ p: 2 }}>Vaccine Information</Typography>
+                            </CardContent>
+
+                        </Card>
+                    </CardActionArea>
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Card sx={{ height: 200, p: 2 }}>
+                        <CardContent sx={{ p: 2, textAlign: "center" }}>
+                            <img src='https://www.hopkinsmedicine.org/_images/_svg/virus-blue.svg' alt='COVID' height={125} width={125} />
+                            <Typography variant="h5" component="div" sx={{ p: 2 }}>Locations</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                    <Card sx={{ height: '200px', p: 2 }}>
+                        <CardContent sx={{ p: 2, textAlign: "center" }}>
+                            <img src='https://www.hopkinsmedicine.org/_images/_svg/health-blue.svg' alt='COVID' height={125} width={125} />
+                            <Typography variant="h5" component="div" sx={{ p: 2 }}>Appoinments</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Container >
+        </div>
+    )
+}
+
+export default Caresafely

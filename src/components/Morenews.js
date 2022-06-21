0bef7da3f34031b0640792dd9f1bc69db4fe5c2c
Mehulpatangya <mehuljainind@gmail.com>
2022-06-21 23:12:24 +0530

    more news data added

--- src/components/Morenews.js
+++ src/components/Morenews.js
@@ -1,11 +1,35 @@
-import { Container } from '@mui/material'
+import {Button, Container, Typography, Grid } from '@mui/material'
 import React from 'react'
 
 function Morenews() {
-  return (
-    <Container >
-    </Container>
-  )
+    return (
+        <Container maxWidth='lg' sx={{ p:8, textAlign: 'center' }}>
+
+            <Typography component="div" sx={{ pb: 4, fontSize: '2.2rem' }}>More News</Typography>
+            <Grid container spacing={2} sx={{alignItems: 'center'}}>
+                <Grid item xs={6}>
+                    <div style={{padding: 2}}>
+                        <Button >News & Publication</Button><br/>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <div style={{padding: 2}}>
+                        <Button>News & Publication</Button><br/>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <div style={{padding: 2}}>
+                        <Button>News & Publication</Button><br/>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <div style={{padding: 2}}>
+                        <Button>News & Publication</Button>
+                    </div>
+                </Grid>
+            </Grid>
+        </Container>
+    )
 }
 
 export default Morenews

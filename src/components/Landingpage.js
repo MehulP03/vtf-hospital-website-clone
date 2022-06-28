81ddd451171a7d6a8200bb966495e1b15ba39959
Mehul Patangya <57245913+MehulP03@users.noreply.github.com>
2022-06-28 22:28:50 +0530

    logo updated

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Landingpage() {
   return (
-    <div>
+    <div style={{marginTop: '65px'}}>
         <img src='../assets/unknown.png' alt='background' height={414} width={1519}/>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-        <AppBar position="static">
+        <AppBar position="fixed" sx={{backgroundColor: "#111111"}}>
             <Container maxWidth='xl' sx={{display: 'flex', p:1}}>
                 <div>
-                <img src='../assets/logo.png' alt='logo' height={80} width={100}/>
+                  <img src='/assets/logo.svg' alt='logo' height={65} width={100}/>
+                {/* <img src='../assets/logo.png' alt='logo' height={80} width={100}/> */}
                 </div>
                 <Typography variant="h5" component='div' sx={{p:2}}>
-                MyHealthCareGroup
+                My Health Care Group
                 </Typography>
 
             </Container>
